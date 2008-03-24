59d7da9b94f4efb6f36a8dce68c402fcd5d6aeec
James Darling <abscond@Sam.local>
2008-03-24 19:28:09 +0000

    added markdown hard breaks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,4 +18,14 @@ module ApplicationHelper
   def url_for_item_comments_preview(item)
     url_for_item_comments(item) + '/preview'
   end
+  
+  def markdown(string)
+    return '' if string.blank?
+    returning html = BlueCloth.new(string).to_html do
+      require 'hpricot'
+      Hpricot(html).search("//p").each do |p|
+        html.sub!(p.to_s, p.to_s.gsub(%r{\n+}, "\n<br/>\n"))
+      end
+    end
+  end
 end
