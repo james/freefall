4ff48154ef65e71c2b71aa6393eb2c486262dcf6
James Darling <abscond@Sam.config>
2008-05-26 17:56:19 +0100

    fix tweet.published?

--- app/models/items/tweet.rb
+++ app/models/items/tweet.rb
@@ -3,11 +3,12 @@ class Tweet < Item
   #This chunk ignores the SQL default so that it can use a more intelligent ruby default
   before_save :set_auto_publish
   def after_initialize
-    self.published = nil
+    self.published = nil if self.new_record?
   end
   def set_auto_publish
-    return true if self.published != nil
-    self.published = (text[-1] == 46)
+    if self.published == nil
+      self.published = (text[-1] == 46)
+    end
     true
   end
   
