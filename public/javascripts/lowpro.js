302b3f36220b11a344e4493cd7bc145609aa21f0
James Darling <abscond@Sam.config>
2008-06-15 12:24:58 +0100

    js and elsewhere

--- public/javascripts/lowpro.js
+++ public/javascripts/lowpro.js
@@ -1,8 +1,8 @@
 LowPro = {};
 LowPro.Version = '0.5';
-LowPro.CompatibleWithPrototype = '1.6.0';
+LowPro.CompatibleWithPrototype = '1.6';
 
-if (Prototype.Version != LowPro.CompatibleWithPrototype && console && console.warn)
+if (Prototype.Version.indexOf(LowPro.CompatibleWithPrototype) != 0 && window.console && window.console.warn)
   console.warn("This version of Low Pro is tested with Prototype " + LowPro.CompatibleWithPrototype + 
                   " it may not work as expected with this version (" + Prototype.Version + ")");
 
@@ -15,19 +15,19 @@ DOM = {};
 // DOMBuilder for prototype
 DOM.Builder = {
 	tagFunc : function(tag) {
-	  return function() {
-	    var attrs, children; 
-	    if (arguments.length>0) { 
-	      if (arguments[0].nodeName || 
-	        typeof arguments[0] == "string") 
-	        children = arguments; 
-	      else { 
-	        attrs = arguments[0]; 
-	        children = Array.prototype.slice.call(arguments, 1); 
-	      };
-	    }
-	    return DOM.Builder.create(tag, attrs, children);
-	  };
+    return function() {
+     var attrs, children;
+     if (arguments.length>0) {
+       if (arguments[0].constructor == Object) {
+         attrs = arguments[0];
+         children = Array.prototype.slice.call(arguments, 1);
+       } else {
+         children = arguments;
+       };
+       children = $A(children).flatten()
+     }
+     return DOM.Builder.create(tag, attrs, children);
+    };
   },
 	create : function(tag, attrs, children) {
 		attrs = attrs || {}; children = children || []; tag = tag.toLowerCase();
@@ -109,6 +109,14 @@ Event.addBehavior = function(rules) {
   
 };
 
+Event.delegate = function(rules) {
+  return function(e) {
+      var element = $(e.element());
+      for (var selector in rules)
+        if (element.match(selector)) return rules[selector].apply(this, $A(arguments));
+    }
+}
+
 Object.extend(Event.addBehavior, {
   rules : {}, cache : [],
   reassignAfterAjax : false,
@@ -119,7 +127,7 @@ Object.extend(Event.addBehavior, {
       var observer = rules[selector];
       var sels = selector.split(',');
       sels.each(function(sel) {
-        var parts = sel.split(/:(?=[a-z:]+$)/), css = parts.shift(), event = parts.join(':');
+        var parts = sel.split(/:(?=[a-z]+$)/), css = parts[0], event = parts[1];
         $$(css).each(function(element) {
           if (event) {
             observer = Event.addBehavior._wrapObserver(observer);
@@ -252,6 +260,8 @@ var Behavior = {
   }
 };
 
+
+
 Remote = Behavior.create({
   initialize: function(options) {
     if (this.element.nodeName == 'FORM') new Remote.Form(this.element, options);
@@ -264,11 +274,19 @@ Remote.Base = {
     this.options = Object.extend({
       evaluateScripts : true
     }, options || {});
+    
+    this._bindCallbacks();
   },
   _makeRequest : function(options) {
     if (options.update) new Ajax.Updater(options.update, options.url, options);
     else new Ajax.Request(options.url, options);
     return false;
+  },
+  _bindCallbacks: function() {
+    $w('onCreate onComplete onException onFailure onInteractive onLoading onLoaded onSuccess').each(function(cb) {
+      if (Object.isFunction(this.options[cb]))
+        this.options[cb] = this.options[cb].bind(this);
+    }.bind(this));
   }
 }
 
